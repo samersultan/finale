8f4e38ddb11ba3ff86243d0e2374896141bd4cf3
Scott McCauley <scott@ascottmccauley.com>
2013-03-28 11:19:46 -0400

    initial commit

---
+++ config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/scss"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
+fonts_dir = "assets/font"
+
+relative_assets = true
+preferred_syntax = :scss
+
+environment = :production
+
+output_style = (environment == :production) ? :compressed : :expanded
+
+line_comments = (environment == :production) ? false : true
