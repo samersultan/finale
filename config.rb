9e3245882701c12ff8285c57b445a3ce5d34dfa9
Scott McCauley <scott@ascottmccauley.com>
2013-04-20 11:05:15 -0400

    modifying for updates to parent

--- config.rb
+++ config.rb
@@ -9,7 +9,7 @@ fonts_dir = "assets/font"
 relative_assets = true
 preferred_syntax = :scss
 
-environment = :production
+environment = :development
 
 output_style = (environment == :production) ? :compressed : :expanded
 
